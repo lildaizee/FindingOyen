a1f7c23c536730b44ba5d1b38332e6522184aea0
AmirulIsmail <91870087+AmirulIsmail@users.noreply.github.com>
2022-07-11 20:12:51 +0800

    Respawn player
    When player fell from map, it will respawn

---
+++ Assets/Script/Respawn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+   // public CharacterController controller;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.transform.position;
+            Physics.SyncTransforms();
+        }
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+
+
+    //    controller.enabled = false;  //Prevents Char controller from overiding transform
+    //    player.transform.position = respawnPoint.transform.position;
+    //    Debug.Log("Triggered \n");
+    //    controller.enabled = true; // Gives control back to char controller
+
+
+    //}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    player.transform.position = respawnPoint.transform.position;
+
+    //}
+}
