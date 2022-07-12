18fe8035dfc79c97a3abe6624214ebfa66bbc5e3
AmirulIsmail <91870087+AmirulIsmail@users.noreply.github.com>
2022-07-12 19:50:32 +0800

    Insert photon
    create room, UI for multiplayer

---
+++ Assets/MultiplayerScript/CreateandJoinRoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
+
+public class CreateandJoinRoom : MonoBehaviourPunCallbacks
+{
+    public InputField createInput;
+    public InputField joinInput;
+
+    public string gameScene;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SceneManager.LoadScene(gameScene);
+        
+    }
+}
