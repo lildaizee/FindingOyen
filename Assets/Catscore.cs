04c5ff504b0e83cdb38d1f38588975a1dca582fc
aleefhyqal <aleefhyqal@gmail.com>
2022-07-13 22:49:55 +0800

    Level 1

---
+++ Assets/Catscore.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catscore : MonoBehaviour
+{
+    public int totalScore;
+    //public AudioClip scoreSound;
+
+    void Start()
+    {
+        //GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //GameFlow.totalCoins += 1;
+            // Debug.Log(GameFlow.totalCoins);
+  
+            //ScoreManager.instance.AddPoint();
+            //AudioSource.PlayClipAtPoint(scoreSound, transform.position);
+            GameFlow.totalCoins += 10;
+            Debug.Log(GameFlow.totalCoins);
+            Destroy(gameObject);
+        }
+    }
+}
