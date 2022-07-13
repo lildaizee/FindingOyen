dd17ae60e7438409778722f45693d48da533d948
nuramyra <96581651+lildaizee@users.noreply.github.com>
2022-07-13 22:55:09 +0800

    feat: character selection scene

--- Assets/Script/Player/CharacterSelection.cs
+++ Assets/Script/Player/CharacterSelection.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject[] characterList;
+    private int index;
+
+    private void Start()
     {
-        
+        index = PlayerPrefs.GetInt("CharacterSelected");
+
+        characterList = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+            characterList[i] = transform.GetChild(i).gameObject;
+
+        foreach (GameObject go in characterList)
+            go.SetActive(false);
+
+        if (characterList[index])
+            characterList[index].SetActive(true);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleLeft()
     {
-        
+        //Toggle off the current model
+        characterList[index].SetActive(false);
+
+        index--;
+        if (index < 0)
+        {
+            index = characterList.Length - 1;
+
+        }
+
+        //Toggle on the new model
+        characterList[index].SetActive(true);
+    }
+
+    public void ToggleRight()
+    {
+        //Toggle off the current model
+        characterList[index].SetActive(false);
+
+        index++;
+        if (index == characterList.Length)
+        {
+            index = 0;
+
+        }
+
+        //Toggle on the new model
+        characterList[index].SetActive(true);
+    }
+
+    public void ConfirmButton()
+    {
+        PlayerPrefs.SetInt("CharacterSelected", index);
+        SceneManager.LoadScene("Multiplayer");
     }
 }
