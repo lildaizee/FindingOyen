58de2b26d54b46c2ae263824df22ec719c9626bf
scarffy <34879344+scarffy@users.noreply.github.com>
2022-07-15 09:45:58 +0800

    fix(network): game to start when 3 players join

--- Assets/Script/Multiplayer/LobbyManager.cs
+++ Assets/Script/Multiplayer/LobbyManager.cs
@@ -151,7 +151,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 2)
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 3)
         {
             playButton.SetActive(true);
         }
